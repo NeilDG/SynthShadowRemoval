895206bd3edc3f14de72df3c99e14044435a48c7
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2021-11-28 19:21:19 +0800

    Modified VM scripts

--- gcloud_main.py
+++ gcloud_main.py
@@ -1,25 +1,9 @@
 #BASE SCRIPT FOR RUNNING IN GLOUD
 import os
 def main():
-    os.system("python \"render_maps_main.py\" --server_config=3 --num_workers=4 --batch_size=256 "
-              "--img_to_load=-1 --load_previous=0 --test_mode=0 --num_blocks=6 "
-              "--net_config=1 --l1_weight=0.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=0 "
-              "--version_name=\"rgb2smoothness_v1.05\" --iteration=1 --map_choice=\"smoothness\"")
-
-    os.system("python \"render_maps_main.py\" --server_config=3 --num_workers=4 --batch_size=256 "
-              "--img_to_load=-1 --load_previous=0 --test_mode=0 --num_blocks=6 "
-              "--net_config=1 --l1_weight=0.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=1 "
-              "--version_name=\"rgb2smoothness_v1.05\" --iteration=2 --map_choice=\"smoothness\"")
-
-    os.system("python \"render_maps_main.py\" --server_config=3 --num_workers=4 --batch_size=512 "
-              "--img_to_load=-1 --load_previous=0 --test_mode=0 --num_blocks=0 "
-              "--net_config=2 --l1_weight=0.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=0 "
-              "--version_name=\"rgb2smoothness_v1.06\" --iteration=1 --map_choice=\"smoothness\"")
-
-    os.system("python \"render_maps_main.py\" --server_config=3 --num_workers=4 --batch_size=512 "
-              "--img_to_load=-1 --load_previous=0 --test_mode=0 --num_blocks=0 "
-              "--net_config=2 --l1_weight=0.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=1 "
-              "--version_name=\"rgb2smoothness_v1.06\" --iteration=2 --map_choice=\"smoothness\"")
+    os.system("python \"render_segment_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=0 --test_mode=0 --batch_size=512 "
+              "--net_config=2 --l1_weight=10.0 --use_bce=0 --min_epochs=120 "
+              "--version_name=\"rgb2smoothness_v1.08\" --iteration=1 --map_choice=\"smoothness\"")
 
 if __name__ == "__main__":
     main()
--- pc_main.py
+++ pc_main.py
@@ -4,10 +4,13 @@ def main():
     #           "--likeness_weight=10.0 --use_bce=0 --use_lpips=1 "
     #           "--version_name=\"embedding_v1.00\" --iteration=1")
     # #
-
-    os.system("python \"render_segment_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --test_mode=0 --batch_size=256 --num_blocks=6 "
-              "--net_config=4 --l1_weight=10.0 --use_bce=0 "
-              "--version_name=\"rgb2smoothness_v1.07\" --iteration=1 --map_choice=\"smoothness\"")
+    os.system("python \"render_segment_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=0 --test_mode=0 --batch_size=256 "
+              "--net_config=2 --l1_weight=10.0 --use_bce=0 --min_epochs=120 "
+              "--version_name=\"rgb2smoothness_v1.08\" --iteration=1 --map_choice=\"smoothness\"")
+    #
+    # os.system("python \"render_segment_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=0 --test_mode=0 --batch_size=256 --num_blocks=6 "
+    #           "--net_config=2 --l1_weight=10.0 --use_bce=0 "
+    #           "--version_name=\"rgb2specular_v1.08\" --iteration=1 --map_choice=\"specular\"")
 
     # os.system("python \"render_maps_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=1 --test_mode=0 --batch_size=128 --num_blocks=9 "
     #           "--net_config=1 --l1_weight=0.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=1 "
@@ -34,7 +37,7 @@ def main():
     #           "--version_name=\"maps2rgb_v1.01\" --iteration=3")
     #
     # os.system("python \"relighting_main.py\" --num_workers=12 --img_to_load=-1 --load_previous=0 --test_mode=0 --batch_size=256 --num_blocks=0 "
-    #           "--net_config=2 --l1_weight=1.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_bce=1 "
+    #           "--net_config=2 --l1_weight=1.0 --lpip_weight=10.0 --ssim_weight=0.0 --use_0bce=1 "
     #           "--version_name=\"maps2rgb_v1.01\" --iteration=4")
 
 if __name__ == "__main__":
