95e2f329fd834584bd017c1fdbc44e5795d5e462
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-01-22 17:23:04 +0800

    Updated server scripts

--- gcloud_main.py
+++ gcloud_main.py
@@ -4,68 +4,68 @@ import os
 def train_albedo():
     #train albedo
     os.system("python \"albedo_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --use_mask=0 --mode=azimuth --min_epochs=100 --light_angle=0 "
-              "--version_name=\"rgb2albedo_v7.14\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --use_mask=0 --mode=azimuth --min_epochs=100 --light_angle=0 "
+              "--version_name=\"rgb2albedo_v7.14\" --iteration=6")
 
     os.system("python \"albedo_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --use_mask=0 --mode=azimuth --min_epochs=200 --light_angle=36 "
-              "--version_name=\"rgb2albedo_v7.14\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --use_mask=0 --mode=azimuth --min_epochs=200 --light_angle=36 "
+              "--version_name=\"rgb2albedo_v7.14\" --iteration=6")
 
     os.system("python \"albedo_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --use_mask=0 --mode=azimuth --min_epochs=300 --light_angle=72 "
-              "--version_name=\"rgb2albedo_v7.14\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --use_mask=0 --mode=azimuth --min_epochs=300 --light_angle=72 "
+              "--version_name=\"rgb2albedo_v7.14\" --iteration=6")
 
     os.system("python \"albedo_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --use_mask=0 --mode=azimuth --min_epochs=400 --light_angle=108 "
-              "--version_name=\"rgb2albedo_v7.14\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --use_mask=0 --mode=azimuth --min_epochs=400 --light_angle=108 "
+              "--version_name=\"rgb2albedo_v7.14\" --iteration=6")
 
     os.system("python \"albedo_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --use_mask=0 --mode=azimuth --min_epochs=500 --light_angle=144 "
-              "--version_name=\"rgb2albedo_v7.14\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --use_mask=0 --mode=azimuth --min_epochs=500 --light_angle=144 "
+              "--version_name=\"rgb2albedo_v7.14\" --iteration=6")
 
 def train_shading():
     #train shading
     os.system("python \"shading_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=100 --light_angle=0 "
-              "--version_name=\"rgb2shading_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=100 --light_angle=0 "
+              "--version_name=\"rgb2shading_v8.04\" --iteration=6")
 
     os.system("python \"shading_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=200 --light_angle=36 "
-              "--version_name=\"rgb2shading_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=200 --light_angle=36 "
+              "--version_name=\"rgb2shading_v8.04\" --iteration=6")
 
     os.system("python \"shading_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=300 --light_angle=72 "
-              "--version_name=\"rgb2shading_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=300 --light_angle=72 "
+              "--version_name=\"rgb2shading_v8.04\" --iteration=6")
 
     os.system("python \"shading_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=400 --light_angle=108 "
-              "--version_name=\"rgb2shading_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=400 --light_angle=108 "
+              "--version_name=\"rgb2shading_v8.04\" --iteration=6")
 
     os.system("python \"shading_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=500 --light_angle=144 "
-              "--version_name=\"rgb2shading_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=500 --light_angle=144 "
+              "--version_name=\"rgb2shading_v8.04\" --iteration=6")
 
 def train_shadow():
     # train shadow
     os.system("python \"shadowmap_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=100 --light_angle=0 "
-              "--version_name=\"rgb2shadow_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=100 --light_angle=0 "
+              "--version_name=\"rgb2shadow_v8.04\" --iteration=6")
 
     os.system("python \"shadowmap_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=200 --light_angle=36 "
-              "--version_name=\"rgb2shadow_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=200 --light_angle=36 "
+              "--version_name=\"rgb2shadow_v8.04\" --iteration=6")
 
     os.system("python \"shadowmap_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=300 --light_angle=72 "
-              "--version_name=\"rgb2shadow_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=300 --light_angle=72 "
+              "--version_name=\"rgb2shadow_v8.04\" --iteration=6")
 
     os.system("python \"shadowmap_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=400 --light_angle=108 "
-              "--version_name=\"rgb2shadow_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=400 --light_angle=108 "
+              "--version_name=\"rgb2shadow_v8.04\" --iteration=6")
 
     os.system("python \"shadowmap_main.py\" --server_config=3 --num_workers=8 --img_to_load=-1 --load_previous=1 --test_mode=0 --patch_size=32 --batch_size=2048 --net_config=2 --num_blocks=0 "
-              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=0 --mode=azimuth --min_epochs=500 --light_angle=144 "
-              "--version_name=\"rgb2shadow_v8.04\" --iteration=5")
+              "--l1_weight=10.0 --lpip_weight=1.0 --ssim_weight=0.0 --adv_weight=1.0 --use_bce=1 --mode=azimuth --min_epochs=500 --light_angle=144 "
+              "--version_name=\"rgb2shadow_v8.04\" --iteration=6")
 
 def main():
     train_albedo()
