3a1b1fb1d101d298c94f568cd703521cfa82a06f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-04-20 22:24:02 +0800

    Bug fixes and optimization

--- transforms/cyclegan_transforms.py
+++ transforms/cyclegan_transforms.py
@@ -15,21 +15,19 @@ class CycleGANTransform(nn.Module):
         super(CycleGANTransform, self).__init__()
 
         self.patch_size = (opts.patch_size, opts.patch_size)
-        self.grid_size = (opts.patch_size / 16, opts.patch_size / 16)
 
-        self.transform_op = kornia.augmentation.PatchSequential(
+        self.transform_op = kornia.augmentation.ImageSequential(
             kornia.augmentation.RandomVerticalFlip(p=0.5),
             kornia.augmentation.RandomHorizontalFlip(p=0.5),
-        grid_size=(self.grid_size),
-        patchwise_apply=False,
-        same_on_batch=True)
+            same_on_batch=True)
 
         self.patch_extract_op = kornia.contrib.ExtractTensorPatches(window_size=self.patch_size, stride=self.patch_size)
 
 
     def forward(self, x):
-        out_tensor = self.transform_op(x)
-        out_tensor = self.patch_extract_op(out_tensor)
+        out_tensor = self.patch_extract_op(x)
         out_tensor = torch.flatten(out_tensor, 0, 1)
+        out_tensor = self.transform_op(out_tensor)
+        
         # print("Shape: ", np.shape(out_tensor))
         return out_tensor
