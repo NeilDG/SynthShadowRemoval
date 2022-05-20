aa86beabccb727d74bde181d5c76ec795c722772
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-05-20 13:53:28 +0800

    Update CCS cloud script

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    os.system("python \"relighting_main.py\" --server_config=2 --cuda_device=cuda:0 --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --net_config=3 --num_blocks=6 "
+    os.system("python \"relighting_main.py\" --server_config=2 --cuda_device=cuda:1 --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"maps2rgb_rgb2maps_v4.12\" --iteration=9")
 
 if __name__ == "__main__":
