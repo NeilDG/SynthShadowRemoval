a2e848043b56e384a42c3e049eb757bf691c1f4c
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-06-14 20:32:25 +0800

    Update ccs2_main.py

--- ccs2_main.py
+++ ccs2_main.py
@@ -1,57 +1,57 @@
 import os
 
 def main():
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=13 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=14 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=15 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=16 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=17 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=18 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=5 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=6 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=7 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=8 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
 
-    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=128 --min_epochs=20 "
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=256 --min_epochs=20 "
               "--net_config=3 --num_blocks=6 "
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv6.06\" --iteration=9 "
               "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\"")
