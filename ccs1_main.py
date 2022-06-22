9f8a549955008117bba273e7d063d78c4f46ce08
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-06-22 23:06:43 +0800

    Update ccs1_main.py

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,20 +1,20 @@
 import os
 
 def main():
-    os.system("python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=1 --test_mode=0 --net_config=2 --num_blocks=0 "
+    os.system("python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=0 --test_mode=0 --net_config=2 --num_blocks=0 "
         "--patch_size=32 --img_per_iter=32 --batch_size=1 --plot_enabled=1 "
         "--min_epochs=30 --g_lr=0.0002 --d_lr=0.0002 --version_name=\"synth2rgb_v5.00\" --iteration=3")
 
     os.system(
-        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=1 --test_mode=0 --net_config=2 --num_blocks=0 "
+        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=0 --test_mode=0 --net_config=2 --num_blocks=0 "
         "--patch_size=32 --img_per_iter=32 --batch_size=1 --plot_enabled=1 "
         "--min_epochs=30 --g_lr=0.0002 --d_lr=0.0002 --version_name=\"synth2rgb_v5.00\" --iteration=4")
     os.system(
-        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=1 --test_mode=0 --net_config=2 --num_blocks=0 "
+        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=0 --test_mode=0 --net_config=2 --num_blocks=0 "
         "--patch_size=32 --img_per_iter=32 --batch_size=1 --plot_enabled=1 "
         "--min_epochs=30 --g_lr=0.0002 --d_lr=0.0002 --version_name=\"synth2rgb_v5.00\" --iteration=5")
     os.system(
-        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=1 --test_mode=0 --net_config=2 --num_blocks=0 "
+        "python \"cyclegan_main.py\" --server_config=2 --img_to_load=-1 --load_previous=0 --test_mode=0 --net_config=2 --num_blocks=0 "
         "--patch_size=32 --img_per_iter=32 --batch_size=1 --plot_enabled=1 "
         "--min_epochs=30 --g_lr=0.0002 --d_lr=0.0002 --version_name=\"synth2rgb_v5.00\" --iteration=6")
 
