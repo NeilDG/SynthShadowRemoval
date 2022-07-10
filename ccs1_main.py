7df27ed8b19bbbc5df0f0320b84c5d4834f213cd
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-07-10 21:21:31 +0800

    Update ccs1_main.py

--- ccs1_main.py
+++ ccs1_main.py
@@ -6,5 +6,70 @@ def main():
               "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=5 "
               "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
 
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=6 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=7 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=8 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=9 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=10 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=11 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=12 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=13 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=14 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=15 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=16 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=17 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
+    os.system("python \"iid_train.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --load_previous=0 --test_mode=0 --patch_size=64 --batch_size=192 --min_epochs=40 "
+              "--net_config=4 --num_blocks=4 "
+              "--plot_enabled=0 --debug_mode=0 --version_name=\"iid_networkv8.04\" --iteration=18 "
+              "--unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" --da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --albedo_mode=2")
+
 if __name__ == "__main__":
     main()
