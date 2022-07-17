51b0e1a0efc7e5977dda9b92f28b61f71b0820a9
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-07-17 11:14:27 +0800

    Checkpoint for V8.XX.XX checking

--- iid_test.py
+++ iid_test.py
@@ -39,6 +39,7 @@ parser.add_option('--mode', type=str, default="azimuth")
 parser.add_option('--input_path', type=str)
 parser.add_option('--output_path', type=str)
 parser.add_option('--img_size', type=int, default=(256, 256))
+parser.add_option('--unlit_checkpt_file', type=str, default="")
 
 def measure_performance():
     visdom_reporter = plot_utils.VisdomReporter()
@@ -300,6 +301,8 @@ def main(argv):
     torch.manual_seed(manualSeed)
     np.random.seed(manualSeed)
 
+    plot_utils.VisdomReporter.initialize()
+
     device = torch.device(opts.cuda_device if (torch.cuda.is_available()) else "cpu")
     print("Device: %s" % device)
 
@@ -350,7 +353,7 @@ def main(argv):
         input_tensor = tensor_utils.load_metric_compatible_img(input_path, cv2.COLOR_BGR2RGB, True, True, opts.img_size).to(device)
         input_tensor = normalize_op(input_tensor)
 
-        albedo_tensor, shading_tensor, shadow_tensor = trainer.decompose(input_tensor)
+        albedo_tensor, shading_tensor, shadow_tensor, _ = trainer.decompose(input_tensor)
         print(np.shape(albedo_tensor), np.shape(shading_tensor))
 
         albedo_tensor = albedo_tensor * 0.5 + 0.5
--- test_script.py
+++ test_script.py
@@ -3,20 +3,12 @@
 import os
 
 def test_relighting():
-    os.system("python \"iid_test.py\"  --net_config=3 --num_blocks=6 --version_name=\"iid_networkv7.07\" --albedo_mode=0 --iteration=5 "
-              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" "
+    os.system("python \"iid_test.py\"  --net_config=4 --num_blocks=4 --version_name=\"iid_networkv8.04\" --albedo_mode=2 --iteration=5 "
+              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" "
               "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
 
-    os.system("python \"iid_test.py\"  --net_config=3 --num_blocks=6 --version_name=\"iid_networkv7.07\" --albedo_mode=1 --iteration=6 "
-              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" "
-              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
-
-    os.system("python \"iid_test.py\"  --net_config=3 --num_blocks=6 --version_name=\"iid_networkv7.07\" --albedo_mode=1 --iteration=7 "
-              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" "
-              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
-
-    os.system("python \"iid_test.py\"  --net_config=3 --num_blocks=6 --version_name=\"iid_networkv7.07\" --albedo_mode=1 --iteration=8 "
-              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" "
+    os.system("python \"iid_test.py\"  --net_config=4 --num_blocks=4 --version_name=\"iid_networkv8.04\" --albedo_mode=2 --iteration=6 "
+              "--da_enabled=1 --da_version_name=\"embedding_v5.00_5\" --unlit_checkpt_file=\"synth2unlit_v1.00_1.pt\" "
               "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
 
 
