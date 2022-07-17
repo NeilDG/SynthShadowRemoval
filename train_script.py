d780dd70b68b26c482990c0ea38d28ef6f1c03ca
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-07-17 19:10:45 +0800

    Update train_script.py

--- train_script.py
+++ train_script.py
@@ -2,10 +2,10 @@
 
 import os
 def train_relighting():
-    # os.system("python \"iid_train_v2.py\" --server_config=6 --img_to_load=-1 --test_mode=0 "
-    #           "--plot_enabled=0 --version=\"v9.01\" --iteration=15")
+    os.system("python \"iid_train_v2.py\" --server_config=6 --img_to_load=-1 --test_mode=0 "
+              "--plot_enabled=0 --version=\"v9.01\" --iteration=15")
 
-    os.system("python \"iid_train_v2.py\" --server_config=6 --img_to_load=500 --test_mode=0 "
+    os.system("python \"iid_train_v2.py\" --server_config=6 --img_to_load=-1 --test_mode=0 "
               "--plot_enabled=1 --version=\"v9.01\" --iteration=16")
     #
     # os.system("python \"iid_train_v2.py\" --server_config=6 --img_to_load=-1 --test_mode=0 "
