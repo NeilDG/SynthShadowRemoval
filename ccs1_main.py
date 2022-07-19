9276267adab9c22b1c210cba064292f86f00bcd5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-07-19 10:34:34 +0800

    Update ccs1_main.py

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,10 +1,10 @@
 import os
 
 def main():
-    os.system("python \"iid_train_v2.py\" --server_config=5 --img_to_load=-1 --test_mode=0 "
+    os.system("python \"iid_train_v2.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --test_mode=0 "
               "--plot_enabled=0 --version=\"v9.01\" --iteration=7")
 
-    os.system("python \"iid_train_v2.py\" --server_config=5 --img_to_load=-1 --test_mode=0 "
+    os.system("python \"iid_train_v2.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --test_mode=0 "
               "--plot_enabled=0 --version=\"v9.01\" --iteration=8")
 
 if __name__ == "__main__":
