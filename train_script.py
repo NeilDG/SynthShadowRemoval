0124299e4b9fefa2ed294d17f1cbb10f3de98d40
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-09-14 22:44:04 +0800

    Preparation for COARE training

--- train_script.py
+++ train_script.py
@@ -2,18 +2,8 @@
 
 import os
 def train_relighting():
-    # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
-    #           "--plot_enabled=0 --version=\"v21.11\" --iteration=5")
-
-    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=1 --version=\"v26.11\" --iteration=15")
-
-    # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
-    #           "--plot_enabled=0 --version=\"v26.11\" --iteration=13")
-    #
-    # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
-    #           "--plot_enabled=0 --version=\"v26.11\" --iteration=14")
-
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=1000 --debug_run=0 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=5")
 
 def train_domain_adaptation():
     os.system("python \"cyclegan_main.py\" --server_config=4 --img_to_load=-1 --debug_run=0 "
