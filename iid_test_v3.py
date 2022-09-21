96be2146f763ebe7a77f3c9aee5f4ec12d36af8a
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-09-21 20:52:19 +0800

    Setup for testing

--- iid_test_v3.py
+++ iid_test_v3.py
@@ -155,7 +155,7 @@ def main(argv):
         rgb_ws_tensor = rgb_ws.to(device)
         rgb_ns_tensor = rgb_ns.to(device)
 
-        dataset_tester.test_shadow(rgb_ws_tensor, rgb_ns_tensor, "Train", opts)
+        dataset_tester.test_shadow(rgb_ws_tensor, rgb_ns_tensor, "Train", False, opts)
         if (i % 16 == 0):
             break
 
@@ -169,7 +169,7 @@ def main(argv):
         rgb_ws_tensor = rgb_ws.to(device)
         rgb_ns_tensor = rgb_ns.to(device)
 
-        dataset_tester.test_istd_shadow(rgb_ws_tensor, rgb_ns_tensor, opts)
+        dataset_tester.test_istd_shadow(rgb_ws_tensor, rgb_ns_tensor, False, opts)
         # break
 
     dataset_tester.print_ave_shadow_performance("ISTD", opts)
--- test_script.py
+++ test_script.py
@@ -4,7 +4,43 @@ import os
 
 def test_relighting():
     os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=1 --version=\"v27.06\" --iteration=5 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=1 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=2 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=3 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=4 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=5 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=6 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=7 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=8 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=9 "
+              "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
+
+    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+              "--plot_enabled=1 --version=\"v27.02\" --iteration=10 "
               "--input_path=\"E:/IID-TestDataset/GTA/input/\" --output_path=\"E:/IID-TestDataset/GTA/ours/\"")
 
 def main():
