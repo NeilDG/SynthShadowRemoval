12db6a3a23e607fb0497c6bdd59b42c878acf7c9
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-06 09:50:16 +0800

    Bug fixes and improvements.

--- train_script.py
+++ train_script.py
@@ -5,6 +5,15 @@ def train_relighting():
     os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
               "--plot_enabled=0 --version=\"v37.01\" --iteration=6")
 
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
+              "--plot_enabled=0 --version=\"v37.01\" --iteration=1")
+
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
+              "--plot_enabled=0 --version=\"v37.01\" --iteration=2")
+
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
+              "--plot_enabled=0 --version=\"v37.01\" --iteration=9")
+
 def train_domain_adaptation():
     os.system("python \"cyclegan_main.py\" --server_config=4 --img_to_load=-1 --debug_run=0 "
               "--plot_enabled=1 --g_lr=0.0002 --d_lr=0.0002 --version=\"v7.04\" --iteration=5")
