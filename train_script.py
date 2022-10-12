e0fe95490a314e91c8cb040fb7a58da9a0bbc812
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-12 12:06:15 +0800

    Preparatioin for COARE training

--- train_script.py
+++ train_script.py
@@ -15,7 +15,7 @@ def train_relighting():
     #           "--plot_enabled=0 --version=\"v50.02\" --iteration=1")
 
     os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=1 --version=\"v50.03\" --iteration=1")
+              "--plot_enabled=0 --version=\"v50.03\" --iteration=1")
 
     # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
     #           "--plot_enabled=0 --version=\"v50.04\" --iteration=1")
