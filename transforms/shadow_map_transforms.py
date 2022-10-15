5f5ab0f1fe94007aa58015a85c981f9e9bcc7438
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-15 18:22:59 +0800

    Update shadow_map_transforms.py

--- transforms/shadow_map_transforms.py
+++ transforms/shadow_map_transforms.py
@@ -27,8 +27,8 @@ class ShadowMapTransforms():
         shadow_tensor = rgb_tensor_ns - rgb_tensor_ws
 
         shadow_gray = kornia.color.rgb_to_grayscale(shadow_tensor)
-        # shadow_mask = (shadow_gray != 0.0).float()
-        shadow_mask = shadow_gray
+        shadow_mask = (shadow_gray != 0.0).float()
+        # shadow_mask = shadow_gray
 
         if(one_channel == True):
             shadow_tensor = kornia.color.rgb_to_grayscale(shadow_tensor)
