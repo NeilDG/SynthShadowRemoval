f5f208290f9fdd4d6eb5d7815f62d3b328d93d33
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-20 13:03:16 +0800

    Training min epoch increased from 15 to 40.

--- ccs1_main.py
+++ ccs1_main.py
@@ -7,29 +7,5 @@ def main():
     os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
               "--plot_enabled=0 --version=\"v50.07\" --iteration=2")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=3")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=4")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=5")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=6")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=7")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=8")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=9")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=10")
-
 if __name__ == "__main__":
     main()
--- test_script.py
+++ test_script.py
@@ -11,9 +11,9 @@ def test_relighting():
 
     os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
               "--plot_enabled=1 --img_vis_enabled=1 --version=\"v50.07\" --iteration=3")
-
-    os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=1 --img_vis_enabled=1 --version=\"v50.07\" --iteration=4")
+    #
+    # os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
+    #           "--plot_enabled=1 --img_vis_enabled=1 --version=\"v50.07\" --iteration=4")
 
 def main():
     # os.system("python \"processing/dataset_creator.py\"")
