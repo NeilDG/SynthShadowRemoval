dc144e31e8d925b667bb87cb7e3172ae602e0d40
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-21 22:17:57 +0800

    Update iid_server_config.py

--- config/iid_server_config.py
+++ config/iid_server_config.py
@@ -291,7 +291,7 @@ class IIDServerConfig():
             elif (constants.server_config == 2):  # CCS JUPYTER
                 network_config[LOAD_SIZE_KEY] = 8
             elif (constants.server_config == 3):  # GCLOUD
-                network_config[LOAD_SIZE_KEY] = 5
+                network_config[LOAD_SIZE_KEY] = 4
             elif (constants.server_config == 4):  # RTX 2080Ti
                 network_config[LOAD_SIZE_KEY] = 4
             else:  # RTX 3090
