54682c942507e7d1868f07906ba28abf48b186ce
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-21 22:27:32 +0800

    Enabled plotting in Gcloud visdom

--- gcloud3_main.py
+++ gcloud3_main.py
@@ -3,10 +3,10 @@ import os
 
 def main():
     os.system("python \"cyclegan_main.py\" --server_config=3 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.06\" --iteration=2")
+              "--plot_enabled=1 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.06\" --iteration=2")
 
     os.system("python \"cyclegan_main.py\" --server_config=3 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.07\" --iteration=2")
+              "--plot_enabled=1 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.07\" --iteration=2")
 
 
 if __name__ == "__main__":
