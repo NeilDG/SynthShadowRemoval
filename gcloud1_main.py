315af3c809930761b0f218ade0a720288dd31007
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-28 19:48:44 +0800

    Updated GCloud scripts

--- gcloud1_main.py
+++ gcloud1_main.py
@@ -2,11 +2,9 @@
 import os
 
 def main():
-    os.system("python \"cyclegan_main.py\" --server_config=3 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=1 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.03\" --iteration=2")
+    os.system("python \"iid_train_v3.py\" --server_config=3 --cuda_device=\"cuda:0\" --img_to_load=-1 --train_mode=train_shadow_matte "
+              "--plot_enabled=0 --version=\"v58.26\" --iteration=4")
 
-    os.system("python \"cyclegan_main.py\" --server_config=3 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=1 --g_lr=0.0002 --d_lr=0.0002 --version=\"v9.03\" --iteration=5")
 
 
 if __name__ == "__main__":
