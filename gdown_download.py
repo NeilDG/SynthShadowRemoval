76da16940114bd4319ceca621dd12c7b60ea838a
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-29 10:21:58 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -5,18 +5,22 @@ import gdown
 # output = "./v26_base/"
 # gdown.download_folder(url, output=output, use_cookies=False)
 
-#separate files
-#z04
-url = "https://drive.google.com/uc?id=1CdudMs-yb75Zxa42DUQ_MqqU9HN0utwP"
+url = "https://drive.google.com/uc?id=1NLYQA3vn_-tB-YTxG_wp7s5O1T0O9tAm"
 output = "./v26_base/"
 gdown.download(url, output=output)
 
-#z05
-url = "https://drive.google.com/uc?id=1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN"
-output = "./v26_base/"
-gdown.download(url, output=output)
-
-#z06
-url = "https://drive.google.com/uc?id=1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk"
-output = "./v26_base/"
-gdown.download(url, output=output)
+# #separate files
+# #z04
+# url = "https://drive.google.com/uc?id=1CdudMs-yb75Zxa42DUQ_MqqU9HN0utwP"
+# output = "./v26_base/"
+# gdown.download(url, output=output)
+#
+# #z05
+# url = "https://drive.google.com/uc?id=1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN"
+# output = "./v26_base/"
+# gdown.download(url, output=output)
+#
+# #z06
+# url = "https://drive.google.com/uc?id=1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk"
+# output = "./v26_base/"
+# gdown.download(url, output=output)
