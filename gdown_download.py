0d3b0c66e9b5a8450369a99f92ac4b997f9f2e09
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-29 10:16:56 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -9,14 +9,14 @@ import gdown
 #z04
 id = "1CdudMs-yb75Zxa42DUQ_MqqU9HN0utwP"
 output = "./v26_base/"
-gdown.download(id, output=output, use_cookies=False)
+gdown.download(id, output=output)
 
 #z05
 id = "1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN"
 output = "./v26_base/"
-gdown.download(id, output=output, use_cookies=False)
+gdown.download(id, output=output)
 
 #z06
 id = "1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk"
 output = "./v26_base/"
-gdown.download(id, output=output, use_cookies=False)
+gdown.download(id, output=output)
