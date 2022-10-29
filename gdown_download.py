a1518c459e8c1f41d7f782c8aeb369210c7adbff
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-29 10:12:51 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -1,6 +1,22 @@
 import gdown
 
 # a folder
-url = "https://drive.google.com/drive/folders/1CMz2flknC81dX3xlKBgNP34NJAQ651DB"
+# url = "https://drive.google.com/drive/folders/1CMz2flknC81dX3xlKBgNP34NJAQ651DB"
+# output = "./v26_base/"
+# gdown.download_folder(url, output=output, use_cookies=False)
+
+#separate files
+#z04
+url = "https://drive.google.com/file/d/1CdudMs-yb75Zxa42DUQ_MqqU9HN0utwP/"
 output = "./v26_base/"
-gdown.download_folder(url, use_cookies=False)
+gdown.download(url, output=output, use_cookies=False)
+
+#z05
+url = "https://drive.google.com/file/d/1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN/"
+output = "./v26_base/"
+gdown.download(url, output=output, use_cookies=False)
+
+#z06
+url = "https://drive.google.com/file/d/1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk/"
+output = "./v26_base/"
+gdown.download(url, output=output, use_cookies=False)
