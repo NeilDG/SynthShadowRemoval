7c3b1ca1c6ccecfca8f02bae11ca2af916cd32c4
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-10-30 18:56:23 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -1,22 +1,32 @@
 import gdown
 
-# a folder
-# url = "https://drive.google.com/drive/folders/1CMz2flknC81dX3xlKBgNP34NJAQ651DB"
-# output = "./v26_base/"
-# gdown.download_folder(url, output=output, use_cookies=False)
 
-# #separate files
-# #z04
-# url = "https://drive.google.com/uc?id=1CdudMs-yb75Zxa42DUQ_MqqU9HN0utwP"
-# output = "./v26_base/"
-# gdown.download(url, output=output)
-#
-# #z05
-# url = "https://drive.google.com/uc?id=1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN"
-# output = "./v26_base/"
-# gdown.download(url, output=output)
-#
-# #z06
-# url = "https://drive.google.com/uc?id=1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk"
-# output = "./v26_base/"
-# gdown.download(url, output=output)
+#z05
+url = "https://drive.google.com/uc?id=1Cfqv0U6vPiHDaeBWWlOwNucY6A3iX0wN"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
+
+#z06
+url = "https://drive.google.com/uc?id=1CjN64ivdfhtEBy37HvhXEvMa4052ZXkk"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
+
+#z07
+url = "https://drive.google.com/uc?id=1CktT5UVhS7-uXncvOatAaqN4jJoyIZfk"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
+
+#z08
+url = "https://drive.google.com/uc?id=1CmKvgRDImfz0H01AgA-YA2T4iHQJEcs4"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
+
+#z09
+url = "https://drive.google.com/uc?id=1CqtCSzxJ8_IFF8Lj7Cja6qqwmb9ha4ZM"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
+
+#z10
+url = "https://drive.google.com/uc?id=1Cv0lfr6KSKzTwNTC8jEKi699Lzvv2EC5"
+output = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/v26_base/"
+gdown.download(url, output=output)
