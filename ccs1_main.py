aa4cbfda2f89e9ba5ec90d8a21f0a8eda23f31ad
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-08 20:04:43 +0800

    Update ccs1_main.py

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,19 +1,19 @@
 import os
 
 def main():
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 "
               "--plot_enabled=0  --shadow_removal_version=\"v59.01\" "
               "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 "
               "--plot_enabled=0  --shadow_removal_version=\"v59.02\" "
               "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 "
               "--plot_enabled=0  --shadow_removal_version=\"v59.03\" "
               "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 "
               "--plot_enabled=0  --shadow_removal_version=\"v59.04\" "
               "--shadow_removal_iteration=1")
 
