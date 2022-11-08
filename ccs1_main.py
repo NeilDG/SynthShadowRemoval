f58821a534d332a12324375f2e849493fcebce1e
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-08 20:03:13 +0800

    Update ccs1_main.py

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,35 +1,21 @@
 import os
 
 def main():
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=1")
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+              "--plot_enabled=0  --shadow_removal_version=\"v59.01\" "
+              "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=2")
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+              "--plot_enabled=0  --shadow_removal_version=\"v59.02\" "
+              "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=3")
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+              "--plot_enabled=0  --shadow_removal_version=\"v59.03\" "
+              "--shadow_removal_iteration=1")
 
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=4")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=5")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=6")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=7")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=8")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=9")
-
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --debug_run=0 "
-              "--plot_enabled=0 --version=\"v50.07\" --iteration=10")
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 "
+              "--plot_enabled=0  --shadow_removal_version=\"v59.04\" "
+              "--shadow_removal_iteration=1")
 
 if __name__ == "__main__":
     main()
