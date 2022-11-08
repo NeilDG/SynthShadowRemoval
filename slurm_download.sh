d55e6dd78218f8ef4284f791b48613ce7e12554e
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-08 19:22:18 +0800

    Update slurm_download.sh

--- slurm_download.sh
+++ slurm_download.sh
@@ -11,12 +11,13 @@
 #About this script:
 #Download of dataset
 
-#pip install gdown
-#python "gdown_download.py"
+pip install gdown
+python "gdown_download.py"
 
+DATASET_NAME="v32_istd"
 OUTPUT_DIR="/home/jupyter-neil.delgallego/SynthWeather Dataset 10/"
-echo "$OUTPUT_DIR/v31_istd.zip"
+echo "$OUTPUT_DIR/$DATASET_NAME.zip"
 
-zip -F "$OUTPUT_DIR/v31_istd.zip" --out "$OUTPUT_DIR/v31_istd_fixed.zip"
-unzip "$OUTPUT_DIR/v31_istd_fixed.zip" -d $OUTPUT_DIR
-mv "$OUTPUT_DIR/v31_istd_fixed" "$OUTPUT_DIR/v31_istd"
+zip -F "$OUTPUT_DIR/$DATASET_NAME.zip" --out "$OUTPUT_DIR/$DATASET_NAME+fixed.zip"
+unzip "$OUTPUT_DIR/$DATASET_NAME+fixed.zip" -d "$OUTPUT_DIR"
+mv "$OUTPUT_DIR/$DATASET_NAME+fixed" "$OUTPUT_DIR/$DATASET_NAME"
