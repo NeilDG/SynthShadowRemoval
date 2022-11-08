d4c3be5ce75c49f76d37803a97c9fc4dda0b953f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-08 19:18:00 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -2,11 +2,16 @@ import gdown
 
 output_dir = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/"
 
-#V31_ISTD
-#z00
-url = "https://drive.google.com/uc?id=1G6KaUb2Nm4Ta2sTSZo4KAsMwQs048qkX"
-gdown.download(url, output=output_dir)
+# #V31_ISTD
+# #z00
+# url = "https://drive.google.com/uc?id=1G6KaUb2Nm4Ta2sTSZo4KAsMwQs048qkX"
+# gdown.download(url, output=output_dir)
+#
+# #z01
+# url = "https://drive.google.com/uc?id=1G806v7pob1h7a3bGFV_wPHgKlixBRp9Q"
+# gdown.download(url, output=output_dir)
 
-#z01
-url = "https://drive.google.com/uc?id=1G806v7pob1h7a3bGFV_wPHgKlixBRp9Q"
+#V32_ISTD
+#z00
+url = "https://drive.google.com/uc?id=1GRY4HI0MouBTmnMZler_GRZW0OqqHswk"
 gdown.download(url, output=output_dir)
