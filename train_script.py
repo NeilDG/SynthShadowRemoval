5d2dde2991fc8d6bac57a10e33a5aecf0066b539
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-21 18:57:54 +0800

    Reverted COARE cpu workers to 6

--- train_script.py
+++ train_script.py
@@ -2,8 +2,8 @@
 
 import os
 def train_relighting():
-    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=5000 --train_mode=train_shadow_matte "
-              "--plot_enabled=0  --shadow_matte_network_version=\"v60.01\" --shadow_removal_version=\"v58.28\" "
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=train_shadow "
+              "--plot_enabled=0  --shadow_matte_network_version=\"v60.01\" --shadow_removal_version=\"v60.02_places\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
 
 def train_domain_adaptation():
