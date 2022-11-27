3cf906970d8518774afe75d6c270fb29e93ad17f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-11-27 10:32:16 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -6,47 +6,47 @@ output_dir = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/"
 #V34_PLACES
 #z00
 url = "https://drive.google.com/uc?id=1MkYqYi7A452coECRkET8Oi-Hh3H8BY6Y"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z01
 url = "https://drive.google.com/uc?id=1MB-wO5mWooawe0YEFpcuGffxWXRm4tCG"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z02
 url = "https://drive.google.com/uc?id=1MEwOq9dx4-IpD8TJUXy0ny5kuQrVjJ83"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z03
 url = "https://drive.google.com/uc?id=1MI8kDxgvxa9pHkR9kc8-a-G_AxXL3xRX"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z04
 url = "https://drive.google.com/uc?id=1MIN8bxXXWOwK19MxmXjD8gjnbURTsXOm"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z05
 url = "https://drive.google.com/uc?id=1MK1Vgyfoan4evwophFyXptX-wIqC4d-k"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z06
 url = "https://drive.google.com/uc?id=1MRIsMeZDsAP_vpuNlFRu4JssZ52jj5CG"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 
 #z07
 url = "https://drive.google.com/uc?id=1MWLoLYUfRKOfBdXliSOTWCpJY1IVaXcL"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 
 #z08
 url = "https://drive.google.com/uc?id=1MZ9v77M2z2Xel1BSQvR9dLxlO_jrGUEl"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z09
 url = "https://drive.google.com/uc?id=1MZBE-S28MuA861TZC4D_FTZP2uWGFiW0"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
 #z10
 url = "https://drive.google.com/uc?id=1MbyVvqVp2tz9X2yca9V9N_Md1fHL83kf"
-gdown.download(url, output=output_dir)
+gdown.download(url, output=output_dir, use_cookies=False)
 
