b6791821e751cb6aab0a504d7207ee9f72a4ac09
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-12-13 12:11:24 +0800

    Bug fixes in dataset path of SRD.

--- train_script.py
+++ train_script.py
@@ -2,6 +2,10 @@
 
 import os
 def train_relighting():
+    # os.system("python \"iid_train_v3.py\" --server_config=1 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0  --shadow_matte_network_version=\"v60.15_srd\" --shadow_removal_version=\"v60.09_places\" "
+    #           "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
+
     os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
               "--plot_enabled=0  --shadow_matte_network_version=\"v60.15_places\" --shadow_removal_version=\"v60.09_places\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
