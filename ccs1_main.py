6c0389929c94cc9eebdae27a7e2bed36258bdea4
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2022-12-23 20:55:45 +0800

    Updated scripts for exploration on v60.13.XX and V60.20.XX - CCS Cloud training

--- ccs1_main.py
+++ ccs1_main.py
@@ -1,9 +1,9 @@
 import os
 
 def main():
-    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:2\" --img_to_load=-1 --train_mode=\"train_shadow\" "
+    os.system("python \"iid_train_v3.py\" --server_config=2 --cuda_device=\"cuda:0\" --img_to_load=-1 --train_mode=\"train_shadow\" "
               "--plot_enabled=0  --shadow_matte_network_version=\"v60.15_places\" --shadow_removal_version=\"v60.13_places\" "
-              "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
+              "--shadow_matte_iteration=4 --shadow_removal_iteration=4")
 
 if __name__ == "__main__":
     main()
--- train_script.py
+++ train_script.py
@@ -2,12 +2,16 @@
 
 import os
 def train_relighting():
-    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0  --shadow_matte_network_version=\"v60.15_places\" --shadow_removal_version=\"v60.12_places\" "
+    # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
+    #           "--plot_enabled=0  --shadow_matte_network_version=\"v60.15_places\" --shadow_removal_version=\"v60.12_places\" "
+    #           "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
+
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0  --shadow_matte_network_version=\"v60.17_places\" --shadow_removal_version=\"v60.01_srd\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
 
 def train_domain_adaptation():
-    # os.system("python \"cyclegan_main.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
+    # os.system("python \"cyclegan_main.py\" --server_config=5 --img````````````````````````````````````````````````_to_load=-1 --deb````````````````````````````````````````````````ug_run=0 "
     #           "--plot_enabled=0 --g_lr=0.0002 --d_lr=0.0002 --version=\"v10.08\" --iteration=1")
 
     os.system("python \"cyclegan_main.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
