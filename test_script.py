9fbc73bef07af5afa9a03df0fffe148d103703b5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-01-02 23:53:08 +0800

    Bug fixes and improvements.

--- test_script.py
+++ test_script.py
@@ -4,11 +4,11 @@ import os
 
 def test_relighting():
     os.system("python \"iid_test_v3.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=1 --img_vis_enabled=1 --train_mode=train_shadow "
-              "--shadow_matte_network_version=\"v60.25_places\" --shadow_removal_version=\"v60.17_places\" "
+              "--plot_enabled=1 --img_vis_enabled=1 --train_mode=train_shadow_matte "
+              "--shadow_matte_network_version=\"v60.25_places\" --shadow_removal_version=\"v58.28\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
 
-    os.system("python \"benchmark_shadow.py\" --img_to_load=-1")
+    # os.system("python \"benchmark_shadow.py\" --img_to_load=-1")
 
 def main():
     # os.system("python \"processing/dataset_creator.py\"")
