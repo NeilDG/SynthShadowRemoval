102794eaf3c4e3b59285fbfbdcf71945e297e151
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-01-04 17:30:20 +0800

    Bug fixes and updated training script for 2080Ti

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -3,11 +3,7 @@
 import os
 def train_relighting():
     os.system("python \"iid_train_v3.py\" --server_config=4 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0  --shadow_matte_network_version=\"v60.18_places\" --shadow_removal_version=\"v60.01_srd\" "
-              "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
-
-    os.system("python \"iid_train_v3.py\" --server_config=4 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0  --shadow_matte_network_version=\"v60.19_places\" --shadow_removal_version=\"v60.01_srd\" "
+              "--plot_enabled=0  --shadow_matte_network_version=\"v60.16_srd\" --shadow_removal_version=\"v60.15_synshadow\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
 
 def train_domain_adaptation():
--- train_script.py
+++ train_script.py
@@ -3,12 +3,8 @@
 import os
 
 def train_shadow_matte():
-    # os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=10000 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0  --shadow_matte_network_version=\"v60.26_synshadow\" --shadow_removal_version=\"v60.15_synshadow\" "
-    #           "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
-
-    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0  --shadow_matte_network_version=\"v60.16_srd\" --shadow_removal_version=\"v60.15_synshadow\" "
+    os.system("python \"iid_train_v3.py\" --server_config=5 --img_to_load=10000 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0  --shadow_matte_network_version=\"v60.26_synshadow\" --shadow_removal_version=\"v60.15_synshadow\" "
               "--shadow_matte_iteration=4 --shadow_removal_iteration=1")
 
 def train_shadow_removal():
