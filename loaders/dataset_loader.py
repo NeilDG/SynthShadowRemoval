Bug fixes on gaussian noise causing overflow values.

loaders/dataset_loader.py
@@ -273,7 +273,7 @@ def load_shadow_test_dataset():
 
     data_loader = torch.utils.data.DataLoader(
         shadow_datasets.ShadowTrainDataset(img_length, ws_list, ns_list, 2),
-        batch_size=8,
+        batch_size=global_config.test_size,
         num_workers=1,
         shuffle=False
     )
@@ -322,7 +322,7 @@ def load_istd_dataset():
 
     data_loader = torch.utils.data.DataLoader(
         shadow_datasets.ShadowISTDDataset(img_length, ws_istd_list, ns_istd_list, mask_istd_list, 1),
-        batch_size=global_config.load_size,
+        batch_size=global_config.test_size,
         num_workers=1,
         shuffle=False
     )
@@ -338,7 +338,7 @@ def load_srd_dataset():
 
     data_loader = torch.utils.data.DataLoader(
         shadow_datasets.ShadowSRDDataset(img_length, ws_istd_list, ns_istd_list, 1),
-        batch_size=global_config.load_size,
+        batch_size=global_config.test_size,
         num_workers=1,
         shuffle=False
     )

loaders/shadow_datasets.py
@@ -69,9 +69,9 @@ class ShadowTrainDataset(data.Dataset):
                 rgb_ws = rgb_ws * np.random.uniform(1.000, 1.25)
 
             if ("random_noise" in self.network_config["augment_key"]):
-                noise_op = K.RandomGaussianNoise(p=1.0, mean=np.random.uniform(0.0, 1.0), std=np.random.uniform(0.0, 1.0))
-                rgb_ws = torch.squeeze(noise_op(rgb_ws))
-                rgb_ns = torch.squeeze(noise_op(rgb_ns)) #TODO: Observe if it's wise to also add noise to RGB_ns. Hypothesis: Must add noise as well so SMs are clean.
+                noise_op = K.RandomGaussianNoise(p=1.0, mean=np.random.uniform(0.0, 0.25), std=np.random.uniform(0.0, 0.25))
+                rgb_ws = torch.clip(torch.squeeze(noise_op(rgb_ws)), 0.0, 1.0)
+                rgb_ns = torch.clip(torch.squeeze(noise_op(rgb_ns, params=noise_op._params)), 0.0, 1.0) #TODO: Observe if it's wise to also add noise to RGB_ns. Hypothesis: Must add noise as well so SMs are clean.
 
             if (self.transform_config == 1):
                 crop_indices = transforms.RandomCrop.get_params(rgb_ws, output_size=self.patch_size)
@@ -82,9 +82,7 @@ class ShadowTrainDataset(data.Dataset):
 
             rgb_ws, rgb_ns, shadow_map, shadow_matte = self.shadow_op.generate_shadow_map(rgb_ws, rgb_ns, False)
 
-            rgb_ws_gray = kornia.color.rgb_to_grayscale(rgb_ws)
             rgb_ws = self.norm_op(rgb_ws)
-            rgb_ws_gray = self.norm_op(rgb_ws_gray)
             rgb_ns = self.norm_op(rgb_ns)
             shadow_map = self.norm_op(shadow_map)
             shadow_matte = self.norm_op(shadow_matte)
@@ -93,7 +91,6 @@ class ShadowTrainDataset(data.Dataset):
             print("Failed to load: ", self.img_list_a[idx], self.img_list_b[idx])
             print("ERROR: ", e)
             rgb_ws = None
-            rgb_ws_gray = None
             rgb_ns = None
             shadow_map = None
             shadow_matte = None

trainers/shadow_removal_trainer.py
@@ -32,6 +32,7 @@ class ShadowTrainer(abstract_iid_trainer.AbstractIIDTrainer):
         self.shadow_op = shadow_map_transforms.ShadowMapTransforms()
         self.fp16_scaler = amp.GradScaler()  # for automatic mixed precision
         self.common_losses = common_losses.LossRepository(self.gpu_device, self.iteration)
+        self.l1_loss = nn.L1Loss()
 
         self.visdom_reporter = plot_utils.VisdomReporter.getInstance()
         self.load_size = global_config.load_size
@@ -180,8 +181,8 @@ class ShadowTrainer(abstract_iid_trainer.AbstractIIDTrainer):
                 rgb2ns_istd = tensor_utils.normalize_to_01(rgb2ns_istd)
                 target_tensor = tensor_utils.normalize_to_01(target_tensor)
                 istd_ns_test = tensor_utils.normalize_to_01(istd_ns_test)
-                self.losses_dict_t[self.TRAIN_LOSS_KEY].append(self.common_losses.compute_l1_loss(rgb2ns, target_tensor).item())
-                self.losses_dict_t[self.TEST_LOSS_KEY].append(self.common_losses.compute_l1_loss(rgb2ns_istd, istd_ns_test).item())
+                self.losses_dict_t[self.TRAIN_LOSS_KEY].append(self.l1_loss(rgb2ns, target_tensor).item())
+                self.losses_dict_t[self.TEST_LOSS_KEY].append(self.l1_loss(rgb2ns_istd, istd_ns_test).item())
 
     def is_stop_condition_met(self):
         return self.stop_result