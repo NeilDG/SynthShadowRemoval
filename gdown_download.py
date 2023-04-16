827e0c73aec06ea8ced4a3fd9d27fb9e2247f974
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-04-16 09:36:20 +0800

    Update gdown_download.py

--- gdown_download.py
+++ gdown_download.py
@@ -19,46 +19,46 @@ def main(argv):
         output_dir = "/home/jupyter-neil.delgallego/SynthWeather Dataset 10/"
 
     #z00
-    direct_link = "https://drive.google.com/file/d/1liR2C3g9M4u1BhrEJSaC4WkkMZdcTC5p/view?usp=share_link"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
-
-    #z01
-    direct_link = "https://drive.google.com/file/d/1lgL0PqMWiBr-Trvsi7qWkaL88WUPauwP/view?usp=share_link"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
-
-    #z02
-    direct_link = "https://drive.google.com/file/d/1lgqFuTHfXkK-IimryDBUmLfo7s7h9p5w/view?usp=share_link"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
+    # direct_link = "https://drive.google.com/file/d/1liR2C3g9M4u1BhrEJSaC4WkkMZdcTC5p/view?usp=share_link"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
+    #
+    # #z01
+    # direct_link = "https://drive.google.com/file/d/1lgL0PqMWiBr-Trvsi7qWkaL88WUPauwP/view?usp=share_link"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
+    #
+    # #z02
+    # direct_link = "https://drive.google.com/file/d/1lgqFuTHfXkK-IimryDBUmLfo7s7h9p5w/view?usp=share_link"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
 
     #z03
-    direct_link = "https://drive.google.com/file/d/1lgqFuTHfXkK-IimryDBUmLfo7s7h9p5w/view?usp=share_link"
+    direct_link = "https://drive.google.com/file/d/1lhswYavVqAfi6YjjKxldQRqPtNgit4LT/view?usp=share_link"
     id = direct_link.split("/d/")[1].split("/")[0]
     url = "https://drive.google.com/uc?id="+id
     gdown.download(url, output=output_dir, use_cookies=False)
 
-    #z04
-    direct_link = "https://drive.google.com/file/d/1lhuLrwcL_vbifCr5am7je11GLw7p2LUQ/view?usp=share_link"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
-
-    #z05
-    direct_link = "https://drive.google.com/file/d/1liIuI1SmOO9nCsQsELp9uX1My6J8KYr1/view?usp=share_link"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
-
-    #z06
-    direct_link = "https://drive.google.com/file/d/1liYCjJe1IPV-UZUvC2xaimZMwx9mA2Tm/view?usp=sharing"
-    id = direct_link.split("/d/")[1].split("/")[0]
-    url = "https://drive.google.com/uc?id="+id
-    gdown.download(url, output=output_dir, use_cookies=False)
+    # #z04
+    # direct_link = "https://drive.google.com/file/d/1lhuLrwcL_vbifCr5am7je11GLw7p2LUQ/view?usp=share_link"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
+    #
+    # #z05
+    # direct_link = "https://drive.google.com/file/d/1liIuI1SmOO9nCsQsELp9uX1My6J8KYr1/view?usp=share_link"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
+    #
+    # #z06
+    # direct_link = "https://drive.google.com/file/d/1liYCjJe1IPV-UZUvC2xaimZMwx9mA2Tm/view?usp=sharing"
+    # id = direct_link.split("/d/")[1].split("/")[0]
+    # url = "https://drive.google.com/uc?id="+id
+    # gdown.download(url, output=output_dir, use_cookies=False)
 
     #
     # # z07
