f5c0380090db8807c97c056c0ba24e0373339891
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-04-29 13:13:19 +0800

    Update titan2_main.py

--- titan2_main.py
+++ titan2_main.py
@@ -11,7 +11,7 @@ def train_shadow_removal():
 
 def train_img2img():
     os.system("python3 \"train_img2img_main.py\" --server_config=5 --img_to_load=-1 "
-              "--plot_enabled=1 --save_per_iter=250 --network_version=\"synth2istd_v01.00\" --iteration=1")
+              "--plot_enabled=1 --save_per_iter=250 --network_version=\"synth2istd_v01.00\" --iteration=5")
 
 
 def main():
