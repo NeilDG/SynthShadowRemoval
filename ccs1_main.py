c26c18f92e4bbbb48fbd303e0680fee267f076a2
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-05-06 10:36:06 +0800

    Added new training configs.

--- ccs1_main.py
+++ ccs1_main.py
@@ -7,10 +7,10 @@ def train_proper(gpu_device):
     # os.system("python \"train_img2img_main.py\" --server_config=1 --cuda_device=" +gpu_device+ " --img_to_load=-1 "
     #           "--plot_enabled=0 --save_per_iter=250 --network_version=\"synth2istd_v01.00\" --iteration=5")
 
-    os.system("python \"shadow_train_main-3.py\" --server_config=5 --img_to_load=100000 --train_mode=\"train_shadow\" "
+    os.system("python \"shadow_train_main-3.py\" --server_config=1 --img_to_load=100000 --train_mode=\"train_shadow\" "
               "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.26_places\" --iteration=1")
 
-    os.system("python \"shadow_train_main-3.py\" --server_config=5 --img_to_load=50000 --train_mode=\"train_shadow\" "
+    os.system("python \"shadow_train_main-3.py\" --server_config=1 --img_to_load=50000 --train_mode=\"train_shadow\" "
               "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.26_places\" --iteration=1")
 
 
