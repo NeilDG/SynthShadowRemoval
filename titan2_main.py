9a9b3da8e3651ed31dacd8f403d8bc8eb5567883
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-05-12 13:48:37 +0800

    Added script for titan2main training.

--- titan2_main.py
+++ titan2_main.py
@@ -3,7 +3,10 @@
 import os
 def train_shadow_matte():
     os.system("python3 \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.01\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.22_istd\" --iteration=1")
+
+    os.system("python3 \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.23_istd\" --iteration=1")
 
 def train_shadow_removal():
     os.system("python3 \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
@@ -18,7 +21,8 @@ def train_img2img():
 
 
 def main():
-    train_img2img()
+    # train_img2img()
+    train_shadow_matte()
     # os.system("shutdown /s /t 1")
 
 
