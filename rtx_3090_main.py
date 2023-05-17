2899c9534bf08a7a83fc72fa0add1811210783b9
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-05-17 12:13:31 +0800

    Bug fixes and improvements.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -7,9 +7,12 @@ def train_shadow_matte():
     # os.system("python \"shadow_train_main-2.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow_matte\" "
     #           "--plot_enabled=0 --save_every_epoch=5 --epoch_to_load=0 --network_version=\"rgb2sm_v61.32_istd\" --iteration=1")
 
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --network_version=\"rgb2sm_v61.31_istd\" --iteration=1")
+
     # FOR TESTING
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --network_version=\"rgb2sm_v61.test_places\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --network_version=\"rgb2sm_v61.test_places\" --iteration=1")
     #
     # os.system("python \"shadow_train_main-2.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow\" "
     #           "--plot_enabled=1 --save_every_epoch=20 --epoch_to_load=0 --network_version=\"rgb2ns_v61.test_places\" --iteration=1")
@@ -27,8 +30,8 @@ def train_img2img():
               "--plot_enabled=1 --save_per_iter=50 --network_version=\"synth2istd_v01.00\" --iteration=1")
 
 def main():
-    train_shadow_removal()
-    # train_shadow_matte()
+    # train_shadow_removal()
+    train_shadow_matte()
 
     # train_img2img()
     # os.system("shutdown /s /t 1")
