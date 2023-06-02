c9cec222199e60e4cb214ae9f823913bc07704a5
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-02 12:57:09 +0800

    Bug fixes and improvements

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -7,8 +7,8 @@ def train_shadow_matte():
     # os.system("python \"shadow_train_main-2.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow_matte\" "
     #           "--plot_enabled=0 --save_every_epoch=5 --epoch_to_load=0 --network_version=\"rgb2sm_v61.32_istd\" --iteration=1"
 
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --network_version=\"rgb2sm_v61.35_places\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --network_version=\"rgb2sm_v61.35_places\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
               "--plot_enabled=0 --network_version=\"rgb2sm_v61.38_places\" --iteration=1")
