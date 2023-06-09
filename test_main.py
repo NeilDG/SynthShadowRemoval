2a13a050fb08467873308a725f05dfcf31991683
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-09 10:22:35 +0800

    Added new script for CCS cloud.

--- test_main.py
+++ test_main.py
@@ -22,8 +22,8 @@ def test_shadow():
 
     os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
               "--img_vis_enabled=1 --train_mode=train_shadow_matte --dataset_target=all "
-              "--shadow_matte_version=\"rgb2sm_v61.42_places\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
-              "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=0")
+              "--shadow_matte_version=\"rgb2sm_v61.25_places\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
+              "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
 
     # os.system("python \"shadow_test_main-2.py\" --server_config=5 --img_to_load=-1 "
     #           "--img_vis_enabled=1 --train_mode=train_shadow --dataset_target=istd "
