ef7fda1f1f9c78007fe233b096bd1f1b5c5f43af
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-11 19:57:05 +0800

    Update slurm_download.sh

--- slurm_download.sh
+++ slurm_download.sh
@@ -30,15 +30,15 @@ source activate NeilGAN_V2
 #pip install --upgrade --no-cache-dir gdown
 #pip install PyYAML
 
-#if [ $SERVER_CONFIG == 0 ]
-#then
-#  srun python "gdown_download.py" --server_config=$SERVER_CONFIG
-#elif [ $SERVER_CONFIG == 5 ]
-#then
-#  python3 "gdown_download.py" --server_config=$SERVER_CONFIG
-#else
-#  python "gdown_download.py" --server_config=$SERVER_CONFIG
-#fi
+if [ $SERVER_CONFIG == 0 ]
+then
+  srun python "gdown_download.py" --server_config=$SERVER_CONFIG
+elif [ $SERVER_CONFIG == 5 ]
+then
+  python3 "gdown_download.py" --server_config=$SERVER_CONFIG
+else
+  python "gdown_download.py" --server_config=$SERVER_CONFIG
+fi
 
 
 if [ $SERVER_CONFIG == 0 ]
