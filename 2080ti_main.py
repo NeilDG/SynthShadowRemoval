d662a02b8b4e52ee7133061bfb60d8cd559484a6
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-11 16:55:41 +0800

    Added new training scripts for 2080Ti

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -3,10 +3,13 @@
 import os
 def train_shadow_matte():
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.39_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.55_istd+srd\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.42_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.56_istd+srd\" --iteration=1")
+
+    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.57_istd+srd\" --iteration=1")
 
 
 def train_shadow_removal():
