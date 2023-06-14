a1d8a8192b98141d3cbba645719517a2294df157
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-14 21:43:55 +0800

    Added an epoch stopping method if no improvements happen.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -3,12 +3,12 @@
 import os
 
 def train_shadow_matte():
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --network_version=\"rgb2sm_v61.64_istd\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --network_version=\"rgb2sm_v61.64_istd\" --iteration=1")
 
     # FOR TESTING
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=1 --save_per_iter=100 --network_version=\"rgb2sm_v61.64_istd\" --iteration=1")
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=100 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=1 --save_per_iter=1000 --network_version=\"rgb2sm_v61.test\" --iteration=1")
     #
     # os.system("python \"shadow_train_main-2.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow\" "
     #           "--plot_enabled=1 --save_every_epoch=20 --epoch_to_load=0 --network_version=\"rgb2ns_v61.test_places\" --iteration=1")
