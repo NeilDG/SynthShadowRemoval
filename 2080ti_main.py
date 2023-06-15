103aa5a918f5529f55b89f3f326e562f9c7a8bf9
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-15 13:08:41 +0800

    Updated 2080ti training.

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -8,6 +8,9 @@ def train_shadow_matte():
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
               "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.57_istd+srd\" --iteration=1")
 
+    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.64_istd\" --iteration=1")
+
 
 def train_shadow_removal():
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
