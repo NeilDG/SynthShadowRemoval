8eddbe1f424079675cd30986ca5ec9a35f3cb8aa
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-23 11:18:19 +0800

    Added training procedure for 2080Ti.

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -17,22 +17,16 @@ def train_shadow_matte():
 
 def train_shadow_removal():
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.33_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_istd+real\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.34_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_srd+real\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.35_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.44_istd+real\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.36_places\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.37_places\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.38_places\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.44_srd+real\" --iteration=1")
 
 def train_domain_adaptation():
     os.system("python \"cyclegan_main.py\" --server_config=5 --img_to_load=-1 --debug_run=0 "
@@ -40,10 +34,10 @@ def train_domain_adaptation():
 
 
 def main():
+    train_shadow_removal()
     train_shadow_matte()
-    # train_shadow_removal()
     # train_domain_adaptation()
-    # os.system("shutdown /s /t 1")
+    os.system("shutdown /s /t 1")
 
 
 if __name__ == "__main__":
--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -3,24 +3,8 @@
 import os
 
 def train_shadow_matte():
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.80_srd\" --iteration=1")
-    #
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.81_srd\" --iteration=1")
-
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.82_srd\" --iteration=1")
-
-
     os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.82_istd\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.80_istd\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.81_istd\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.83_istd\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
               "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.68_istd\" --iteration=1")
@@ -55,12 +39,12 @@ def train_shadow_removal():
     # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=10000 --train_mode=\"train_shadow\" "
     #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2ns_v61.41_places\" --iteration=1")
 
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=10000 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2ns_v61.42_places\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=10000 --train_mode=\"train_shadow\" "
+    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2ns_v61.42_places\" --iteration=1")
 
     #FOR TESTING
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
-    #           "--plot_enabled=1 --save_per_iter=50 --network_version=\"rgb2ns_v61.00_places\" --iteration=1")
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
+              "--plot_enabled=1 --save_per_iter=50 --network_version=\"rgb2ns_v61.43_srd+real\" --iteration=1")
 
     # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow\" "
     #           "--plot_enabled=1 --save_per_iter=100 --network_version=\"rgb2ns_v61.40_synshadow\" --iteration=1")
@@ -70,8 +54,8 @@ def train_img2img():
               "--plot_enabled=1 --save_per_iter=50 --network_version=\"synth2istd_v01.00\" --iteration=1")
 
 def main():
-    # train_shadow_removal()
-    train_shadow_matte()
+    train_shadow_removal()
+    # train_shadow_matte()
 
     # train_img2img()
     # os.system("shutdown /s /t 1")
