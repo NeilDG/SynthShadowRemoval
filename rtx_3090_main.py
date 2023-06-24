bbe53f9aa89e092faba1a222cdd617bea84dc8e4
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-24 20:53:48 +0800

    Bug fixes and improvements.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -3,18 +3,18 @@
 import os
 
 def train_shadow_matte():
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.68_istd\" --iteration=1")
-    #
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.65_istd\" --iteration=1")
-    #
-    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-    #           "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.69_istd\" --iteration=1")
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.68_istd\" --iteration=1")
+
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.65_istd\" --iteration=1")
+
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.69_istd\" --iteration=1")
 
     # FOR TESTING
-    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=1 --save_per_iter=20 --network_version=\"rgb2sm_v61.52_istd+srd\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=1 --save_per_iter=20 --network_version=\"rgb2sm_v61.52_istd+srd\" --iteration=1")
 
     # #
     # os.system("python \"shadow_train_main-2.py\" --server_config=5 --img_to_load=50 --train_mode=\"train_shadow\" "
@@ -42,11 +42,11 @@ def train_img2img():
               "--plot_enabled=1 --save_per_iter=50 --network_version=\"synth2istd_v01.00\" --iteration=1")
 
 def main():
-    # train_shadow_removal()
+    train_shadow_removal()
     train_shadow_matte()
 
     # train_img2img()
-    # os.system("shutdown /s /t 1")
+    os.system("shutdown /s /t 1")
 
 
 if __name__ == "__main__":
