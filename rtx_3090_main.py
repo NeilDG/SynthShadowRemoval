9105da54a9f06f7792b62c5e1f674faf15b446a1
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-06-25 21:43:16 +0800

    Bug fixes and improvements.

--- rtx_3090_main.py
+++ rtx_3090_main.py
@@ -12,6 +12,9 @@ def train_shadow_matte():
     os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
               "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.69_istd\" --iteration=1")
 
+    os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.50_istd\" --iteration=1")
+
     # FOR TESTING
     # os.system("python \"shadow_train_main.py\" --server_config=5 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
     #           "--plot_enabled=1 --save_per_iter=20 --network_version=\"rgb2sm_v61.52_istd+srd\" --iteration=1")
@@ -42,11 +45,12 @@ def train_img2img():
               "--plot_enabled=1 --save_per_iter=50 --network_version=\"synth2istd_v01.00\" --iteration=1")
 
 def main():
-    train_shadow_removal()
+    # train_shadow_removal()
     train_shadow_matte()
+    train_shadow_removal()
 
     # train_img2img()
-    os.system("shutdown /s /t 1")
+    # os.system("shutdown /s /t 1")
 
 
 if __name__ == "__main__":
--- test_main.py
+++ test_main.py
@@ -29,19 +29,19 @@ def test_shadow():
     #
 
     os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
-              "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=all "
-              "--shadow_matte_version=\"rgb2sm_v61.53_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
+              "--img_vis_enabled=1 --train_mode=train_shadow --dataset_target=all "
+              "--shadow_matte_version=\"rgb2sm_v61.50_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.41_places\" "
               "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
-
-    os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
-              "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=all "
-              "--shadow_matte_version=\"rgb2sm_v61.75_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
-              "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
-
-    os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
-              "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=all "
-              "--shadow_matte_version=\"rgb2sm_v61.76_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
-              "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
+    #
+    # os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
+    #           "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=all "
+    #           "--shadow_matte_version=\"rgb2sm_v61.75_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
+    #           "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
+    #
+    # os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
+    #           "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=all "
+    #           "--shadow_matte_version=\"rgb2sm_v61.76_istd+srd\" --shadow_removal_version=\"rgb2ns_v61.26_places\" "
+    #           "--shadow_matte_iteration=1 --shadow_removal_iteration=1 --load_best=1")
 
     # os.system("python \"shadow_test_main.py\" --server_config=5 --img_to_load=-1 "
     #           "--img_vis_enabled=0 --train_mode=train_shadow_matte --dataset_target=srd "
