6f69defbb6499710cf1d5cb224a492ed3bb69607
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-07-02 10:02:47 +0800

    Bug fixes and improvements in training for 2080Tii

--- 2080ti_main.py
+++ 2080ti_main.py
@@ -2,28 +2,28 @@
 
 import os
 def train_shadow_matte():
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.70_istd\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.70_istd\" --iteration=1")
 
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.71_istd\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.72_istd\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.71_istd\" --iteration=1")
+    #
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.72_istd\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
               "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.73_istd\" --iteration=1")
 
 
 def train_shadow_removal():
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_istd+real\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_istd+real\" --iteration=1")
+    #
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_srd+real\" --iteration=1")
 
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.43_srd+real\" --iteration=1")
-
-    os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.44_istd+real\" --iteration=1")
+    # os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
+    #           "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.44_istd+real\" --iteration=1")
 
     os.system("python \"shadow_train_main.py\" --server_config=2 --img_to_load=-1 --train_mode=\"train_shadow\" "
               "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2ns_v61.44_srd+real\" --iteration=1")
@@ -34,7 +34,7 @@ def train_domain_adaptation():
 
 
 def main():
-    train_shadow_removal()
+    # train_shadow_removal()
     train_shadow_matte()
     # train_domain_adaptation()
     os.system("shutdown /s /t 1")
