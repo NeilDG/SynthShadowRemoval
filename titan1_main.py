f8729855b495d86f9f91f67e5cf3e50b27bcba6f
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-07-12 11:28:21 +0800

    Added new training for titan1

--- titan1_main.py
+++ titan1_main.py
@@ -3,10 +3,16 @@
 import os
 def train_shadow_matte():
     os.system("python3 \"shadow_train_main.py\" --server_config=3 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.57_istd+srd\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.84_istd\" --iteration=1")
 
     os.system("python3 \"shadow_train_main.py\" --server_config=3 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
-              "--plot_enabled=0 --save_per_iter=500 --network_version=\"rgb2sm_v61.47_istd+srd\" --iteration=1")
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.84_srd\" --iteration=1")
+
+    os.system("python3 \"shadow_train_main.py\" --server_config=3 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.85_istd\" --iteration=1")
+
+    os.system("python3 \"shadow_train_main.py\" --server_config=3 --img_to_load=-1 --train_mode=\"train_shadow_matte\" "
+              "--plot_enabled=0 --save_per_iter=1000 --network_version=\"rgb2sm_v61.85_srd\" --iteration=1")
 
 def train_shadow_removal():
     os.system("python3 \"shadow_train_main.py\" --server_config=3 --img_to_load=-1 --train_mode=\"train_shadow\" "
